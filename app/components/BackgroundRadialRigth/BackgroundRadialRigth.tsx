235025d769d1a4b7a9a99be0ea2532efab1465e8
deimov <cortezgonzalo@outlook.es>
2024-02-01 20:20:20 -0300

    ssss

--- app/components/BackgroundRadialRigth/BackgroundRadialRigth.tsx
+++ app/components/BackgroundRadialRigth/BackgroundRadialRigth.tsx
@@ -2,8 +2,8 @@ import Image from "@/node_modules/next/image"
 
 export const BackgroundRadialRigth = () => {
   return (
-    <div className="rigth-0 absolute -z-50 top-0">
-        <Image src="/assets/background-floated-right.png" width={850} height={650} alt="Circle Rigth" />
+    <div className="right-0 absolute -z-50 top-0">
+        <Image src="/assets/background-floated-right.png" width={850} height={650} alt="Circle right" />
     </div>
   )
 }
