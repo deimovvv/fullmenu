e635cc80f290adcc6b868bb6b38ba6adef8ec8a6
deimov <cortezgonzalo@outlook.es>
2024-02-01 20:38:44 -0300

    ssssskk

--- app/components/SectionCaract/SectionCaract.tsx
+++ app/components/SectionCaract/SectionCaract.tsx
@@ -29,8 +29,8 @@ export function SectionCaract(){
             </div>
 
             <div className="grid items-center py-5 md:p-8">
-                {dataFeatures.map(({icon,title, description}) =>(
-                    <Reveal>
+                {dataFeatures.map(({id, icon,title, description}) =>(
+                    <Reveal key={id}> 
                         <div className="grid grid-flow-col gap-5 px-4 py-2 rounded-3xl group hover:bg-blueRadialBlack">
                         <Image src={`/assets/${icon}.png`} alt={title} width={40} height={40} color={"bgwhite"} />
                         </div>
